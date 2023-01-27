c16b22f9aa72171ce63760a284fc4a1d8f1d5e5b
Gandalf <gandalfthewise1@protonmail.com>
2023-01-27 13:42:25 -0500

    fix: dotenv.config path

--- deps.js
+++ deps.js
@@ -1,26 +1,31 @@
 import { generateWallet, generateNewAccount } from '@stacks/wallet-sdk';
 import { StacksTestnet, StacksMainnet } from '@stacks/network';
+import { URL } from 'url'; // in Browser, the URL in native accessible on window
+import path from 'path';
 import dotenv from 'dotenv';
-dotenv.config()
+
+// Load the .env file into process.env
+const __dirname = new URL('.', import.meta.url).pathname;
+dotenv.config({ path: path.resolve(__dirname, './.env') })
 
 // Define the Stacks network (mainnet/testnet)
 export const network = new StacksTestnet();
-
 // Define contract constants
 export const contractAddress = 'ST1DSH0G45GZGGDJP3YVDEXTY4X2ZA89CKB5CZ6PK';
 export const contractNamePrev = "winged-feet-options-v0-7";
 export const contractNameCurr = "winged-feet-options-v0-8";
 
+// Define time constants
 export const secInMs = 1000;
 
 // Generate a wallet and private key from a seed/secretKey
-const password = 'password';
 const secretKey = process.env.TESTNET_WALLET
+const password = 'password';
 let wallet = await generateWallet({
   secretKey,
   password
 });
-wallet = generateNewAccount(wallet);
+wallet = generateNewAccount(wallet); 
 wallet = generateNewAccount(wallet);
 
 export const privKey1 = wallet.accounts[0].stxPrivateKey;
