b572da43611038cb718e736ce17b3305fcd64930
Gandalf <gandalfthewise1@protonmail.com>
2023-02-08 11:30:51 -0600

    fix: buy-options cron job 1 hr later for prev contract

--- cron-jobs/buy-options.js
+++ cron-jobs/buy-options.js
@@ -17,7 +17,6 @@ import { contractAddress, contractNamePrev, contractNameCurr, network, secInMs, 
 // Start cron job, executing every day every minute 11:40-11:55
 cron.schedule('40-55/1 11 * * *', async () => {
 
-  // Read options-for-sale
   let options = {
     contractAddress,
     contractName: contractNameCurr,
@@ -28,17 +27,14 @@ cron.schedule('40-55/1 11 * * *', async () => {
   };
   let optionsForSale = await callReadOnlyFunction(options);
 
-  // Get current STX price
   const price = await redstone.getPrice("STX");
  
-  // Convert price data to format for contract call
   const packageCV = pricePackageToCV({
     timestamp: price.timestamp,
     prices: [{ symbol: price.symbol, value: price.value }]
   });
   const signature = liteSignatureToStacksSignature(price.liteEvmSignature);
 
-  // Make contract call to submit-price-data
   let txOptions = {
     contractAddress,
     contractName: contractNameCurr,
@@ -60,7 +56,64 @@ cron.schedule('40-55/1 11 * * *', async () => {
   let broadcastResponse = await broadcastTransaction(transaction, network);
   console.log(broadcastResponse);
 
-  // Read options-for-sale
+  // options = {
+  //   contractAddress,
+  //   contractName: contractNamePrev,
+  //   functionName: 'get-options-for-sale',
+  //   functionArgs: [],
+  //   network,
+  //   senderAddress: contractAddress,
+  // };
+  // optionsForSale = await callReadOnlyFunction(options);
+
+  // txOptions = {
+  //   contractAddress,
+  //   contractName: contractNamePrev,
+  //   functionName: 'buy-options',
+  //   functionArgs: [
+  //     packageCV.timestamp,
+  //     packageCV.prices,
+  //     bufferCV(signature),
+  //     uintCV(Number(optionsForSale.value))
+  //   ],
+  //   senderKey: privKey2,
+  //   validateWithAbi: true,
+  //   network,
+  //   anchorMode: AnchorMode.Any,
+  //   postConditionMode: PostConditionMode.Allow,
+  // }
+  
+  // transaction = await makeContractCall(txOptions);
+  // broadcastResponse = await broadcastTransaction(transaction, network);
+  // console.log(broadcastResponse);
+},
+{
+  scheduled: true,
+  timezone: 'America/New_York'
+}
+);
+
+// Start cron job, executing every day every minute 12:40-12:55
+cron.schedule('40-55/1 12 * * *', async () => {
+
+  let options = {
+    contractAddress,
+    contractName: contractNamePrev,
+    functionName: 'get-options-for-sale',
+    functionArgs: [],
+    network,
+    senderAddress: contractAddress,
+  };
+  let optionsForSale = await callReadOnlyFunction(options);
+
+  const price = await redstone.getPrice("STX");
+ 
+  const packageCV = pricePackageToCV({
+    timestamp: price.timestamp,
+    prices: [{ symbol: price.symbol, value: price.value }]
+  });
+  const signature = liteSignatureToStacksSignature(price.liteEvmSignature);
+  
   options = {
     contractAddress,
     contractName: contractNamePrev,
@@ -71,7 +124,6 @@ cron.schedule('40-55/1 11 * * *', async () => {
   };
   optionsForSale = await callReadOnlyFunction(options);
 
-  // Make contract call to submit-price-data
   txOptions = {
     contractAddress,
     contractName: contractNamePrev,
